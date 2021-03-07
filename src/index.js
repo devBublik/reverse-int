cb7dfec794fbc52fdff28b6d5c1aca7f7f9f2f88
devbublik <ew.sorokina2011@ya.ru>
2021-03-07 21:09:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const res = (n + '').split('').reverse().join('');
+    if (n != 0 && n > 0) {
+        return res;
+    } else if (n < 0) {
+        let res2 = (n * (-1) + '').split('').reverse().join('');
+        return res2
+    }
 }
